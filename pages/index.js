333d5ebb44d8701deb3cb76827dbad9c3763894b
Shaili Patel <sp2826@njit.edu>
2023-08-06 00:57:54 -0400

    Modified home page

--- pages/index.js
+++ pages/index.js
@@ -1,15 +1,17 @@
 import Head from 'next/head';
 import styles from '../styles/Home.module.css';
+import NavBar from '../components/NavBar'
 
 export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Hello Professor</title>
+        <title>TechFit Gym</title>
       </Head>
-
       <main className={styles.main}>
-        <h1 className={styles.title}>Gym Business</h1>
+      <NavBar />
+        {/* Tagline */}
+        <p className={styles.tagline}>Unlock Your Fitness Potential with Data-Driven Workouts</p>
 
         {/* Hero Section */}
         <section className={styles.hero}>
@@ -27,6 +29,22 @@ export default function Home() {
             />
             <button type="submit">Join Now</button>
           </form>
+        </section>
+
+        {/* Brand Mission */}
+        <section className={styles.mission}>
+          <h2>Our Mission</h2>
+          <p>Empowering individuals to achieve their fitness goals through a data-driven and personalized approach, leveraging advanced technology and expert guidance.</p>
+        </section>
+
+        {/* Brand Core Values */}
+        <section className={styles.values}>
+          <h2>Our Core Values</h2>
+          <ul>
+            <li><strong>Innovation:</strong> Embrace cutting-edge technology to enhance the fitness experience.</li>
+            <li><strong>Personalization:</strong> Tailor workout programs to meet individual needs and goals.</li>
+            <li><strong>Excellence:</strong> Strive for the highest quality in equipment, training, and customer service.</li>
+          </ul>
         </section>
       </main>
     </div>
