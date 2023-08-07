3ca705a12c57e5946121667604cd5c08d3b1dbd2
ysheikh728 <yahya.sheikh1998@gmail.com>
2023-08-07 17:14:26 -0400

    added about and privacy pages

---
+++ pages/about.js
@@ -0,0 +1,35 @@
+// pages/about.js
+
+import Head from 'next/head';
+import styles from '../styles/About.module.css';
+
+export default function About() {
+  return (
+    <div className={styles.container}>
+      <Head>
+        <title>TechnoGym - About</title>
+        <meta name="description" content="Technologically focused gym business" />
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main className={styles.main}>
+        <h1 className={styles.title}>About TechnoGym</h1>
+        <p className={styles.description}>
+          TechnoGym is a technologically focused gym that combines the latest fitness trends with cutting-edge technology.
+        </p>
+        <p className={styles.description}>
+          Our mission is to provide our members with the best fitness experience by leveraging the power of technology to
+          enhance workouts, track progress, and offer personalized training programs.
+        </p>
+        <p className={styles.description}>
+          At TechnoGym, we believe that technology can revolutionize the fitness industry, and we are committed to
+          integrating innovative solutions into every aspect of our gym.
+        </p>
+      </main>
+
+      <footer className={styles.footer}>
+        <p>&copy; {new Date().getFullYear()} TechnoGym. All rights reserved.</p>
+      </footer>
+    </div>
+  );
+}
