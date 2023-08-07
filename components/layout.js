5eb8cdf9e3dcb40698d7bc5beeeafc92c5e8f79c
Radhika Rayala <rr745@njit.edu>
2023-08-07 00:46:36 -0400

    Added updates to Blog branch

--- components/layout.js
+++ components/layout.js
@@ -5,7 +5,7 @@ import utilStyles from '../styles/utils.module.css';
 
 import Link from 'next/link';
 
-const name = 'Radhika';
+const name = '';
 export const siteTitle = 'Next.js Sample Website';
 
 export default function Layout({ children, home }) {
