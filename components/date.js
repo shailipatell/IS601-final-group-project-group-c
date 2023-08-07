b5ffc9bb84da450bdb51d8950d9ef31976cd5977
Radhika Rayala <rr745@njit.edu>
2023-08-07 17:00:37 -0400

    Revert "Added blog"
    This reverts commit 6ff199eef454efafdd0d705d822d9c5cd8dcea91.
    
    reverting changes to include it as part of the branch

--- components/date.js
+++
@@ -1,6 +0,0 @@
-import { parseISO, format } from 'date-fns';
-
-export default function Date({ dateString }) {
-  const date = parseISO(dateString);
-  return <time dateTime={dateString}>{format(date, 'yyyy-mm-dd')}</time>;
-}
