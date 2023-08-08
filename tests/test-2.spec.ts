29cb0cd89ff0ef9891701da29e2ec216d2004bf3
ysheikh728 <yahya.sheikh1998@gmail.com>
2023-08-08 00:42:06 -0400

    updated playwright test for optimization

--- tests/test-2.spec.ts
+++ tests/test-2.spec.ts
@@ -1,59 +1,44 @@
 import { test, expect } from '@playwright/test';
 
-test('Click Join Now Button', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:3001/home');
-  
-  // Act
-  await page.getByRole('button', { name: 'Join Now' }).click();
-  
-  // Assert
-  // Add assertions to verify the expected outcome of clicking the button
-});
+test.describe('Navigation Tests', () => {
+  let page;
 
-test('Click Mission and Core Values Headings', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:3001/home');
-  
-  // Act
-  await page.getByRole('heading', { name: 'Our Mission' }).click();
-  await page.getByRole('heading', { name: 'Our Core Values' }).click();
-  
-  // Assert
-  // Add assertions to verify the expected outcome of clicking the headings
-});
+  // Set up the common test environment
+  test.beforeAll(async ({ browser }) => {
+    page = await browser.newPage();
+    await page.goto('http://localhost:3001/home');
+  });
 
-test('Click About Link and Heading', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:3001/home');
-  
-  // Act
-  await page.getByRole('link', { name: 'About' }).click();
-  await page.getByRole('heading', { name: 'About TechFit Gym' }).click();
-  
-  // Assert
-  // Add assertions to verify the expected outcome of clicking the link and heading
-});
+  // Clean up after all tests have run
+  test.afterAll(async () => {
+    await page.close();
+  });
 
-test('Click Copyright Text and Privacy Policy Link', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:3001/home');
-  
-  // Act
-  await page.getByText('© 2023 TechFit Gym. All rights reserved.').click();
-  await page.getByRole('link', { name: 'Privacy Policy' }).click();
-  
-  // Assert
-  // Add assertions to verify the expected outcome of clicking the text and link
-});
+  test('Click Join Now Button', async () => {
+    await page.getByRole('button', { name: 'Join Now' }).click();
+    // Add assertions to verify the expected outcome of clicking the button
+  });
 
-test('Click Home Link', async ({ page }) => {
-  // Arrange
-  await page.goto('http://localhost:3001/home');
-  
-  // Act
-  await page.getByRole('link', { name: 'Home' }).click();
-  
-  // Assert
-  // Add assertions to verify the expected outcome of clicking the link
+  test('Click Mission and Core Values Headings', async () => {
+    await page.getByRole('heading', { name: 'Our Mission' }).click();
+    await page.getByRole('heading', { name: 'Our Core Values' }).click();
+    // Add assertions to verify the expected outcome of clicking the headings
+  });
+
+  test('Click About Link and Heading', async () => {
+    await page.getByRole('link', { name: 'About' }).click();
+    await page.getByRole('heading', { name: 'About TechFit Gym' }).click();
+    // Add assertions to verify the expected outcome of clicking the link and heading
+  });
+
+  test('Click Copyright Text and Privacy Policy Link', async () => {
+    await page.getByText('© 2023 TechFit Gym. All rights reserved.').click();
+    await page.getByRole('link', { name: 'Privacy Policy' }).click();
+    // Add assertions to verify the expected outcome of clicking the text and link
+  });
+
+  test('Click Home Link', async () => {
+    await page.getByRole('link', { name: 'Home' }).click();
+    // Add assertions to verify the expected outcome of clicking the link
+  });
 });
