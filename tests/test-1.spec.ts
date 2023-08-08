613e3c3cf84f250bd8ea6fc6698d7d54ffd851f5
ysheikh728 <yahya.sheikh1998@gmail.com>
2023-08-08 03:26:10 -0400

    updated playwright test

--- tests/test-1.spec.ts
+++ tests/test-1.spec.ts
@@ -4,7 +4,6 @@ test('test', async ({ page }) => {
   await new Promise(resolve => setTimeout(resolve, 2000)); // Wait for 2 seconds
   await page.goto('http://localhost:3001/home/');
   await page.getByRole('link', { name: 'Home' }).click();
-  await page.getByRole('button', { name: 'Join Now' }).click();
   await page.getByPlaceholder('Enter your email to get started').click();
   await page.getByRole('link', { name: 'About' }).click();
   await page.getByRole('heading', { name: 'About TechFit Gym' }).click();
